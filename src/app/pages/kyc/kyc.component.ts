8047014a2e7035000f7df7a0c8b4c8387903e739
Hjalmers <opensource@rhj.se>
2022-04-10 10:11:39 +0200

    step-6
    Add reactive form

--- src/app/pages/kyc/kyc.component.ts
+++ src/app/pages/kyc/kyc.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectionStrategy, Component, ViewChild} from '@angular/core';
+import {FormBuilder, FormGroup, FormGroupDirective, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-kyc',
   templateUrl: './kyc.component.html',
-  styleUrls: ['./kyc.component.scss']
+  styleUrls: ['./kyc.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class KycComponent implements OnInit {
+export class KycComponent {
 
-  constructor() { }
+  @ViewChild('kycForm') kycForm?: FormGroupDirective;
 
-  ngOnInit(): void {
+  kycFormGroup?: FormGroup;
+  constructor(private _fb: FormBuilder) {
+    this.setupForm();
+  }
+
+  setupForm() {
+    this.kycFormGroup = this._fb.group({
+      firstName: ['', [ Validators.required, Validators.minLength(2) ]],
+    });
   }
 
 }
